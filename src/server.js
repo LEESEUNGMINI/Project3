fa08ae08b864af3ab8e3ebba88e3cea16a1feae3
LEESEUNGMINI <a33990748@gmail.com>
2024-05-08 14:19:49 +0900

    32

--- src/server.js
+++ src/server.js
@@ -34,23 +34,6 @@ app.post("/api/login", loginUser);
 app.get("/api/list", getCourseList) /* 이미지 경로 확인 테스트 */
 
 
-// 서버에서 액세스 토큰을 검증하고 유저 정보를 반환하는 부분
-app.get("/api/user", async (req, res) => {
-  const accessToken = req.headers.authorization.split(" ")[1];
-  try {
-      const decoded = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
-      // 액세스 토큰에서 유저 번호 추출
-      const userId = decoded.no;
-      // 유저 번호를 사용하여 데이터베이스에서 유저 정보를 조회
-      const user = await User.findById(userId);
-      // 클라이언트에게 유저 정보 반환
-      res.json(user);
-  } catch (error) {
-      // 검증 실패 시 클라이언트에게 오류 응답 반환
-      res.status(401).json({ message: "Invalid access token" });
-  }
-});
-
 // 서버 시작
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
