39634bc5ca0a057116a92e4258b363d8ddebdbdc
LEESEUNGMINI <a33990748@gmail.com>
2024-05-08 16:29:59 +0900

    3123

--- src/controller/courseController.js
+++ src/controller/courseController.js
@@ -7,7 +7,7 @@ export const getCourseList = async (req, res) => {
   const no = req.user ? req.user.user_no : null;
   const QUERY = "SELECT c.*, uc.user_courses_no FROM course c LEFT JOIN users_course uc ON c.course_no = uc.course_no AND uc.user_no = ?";
   const result = await db.execute(QUERY, [no]).then(result => result[0]);
-  console.log(result);
+
 
   res.status(200).json({ status: "success", message: "성공", data: result });
 };
