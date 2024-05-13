fad3a38ea76aff93430b815afc00187fcc4c2109
LEESEUNGMINI <a33990748@gmail.com>
2024-05-13 16:01:48 +0900

    내정보수정

--- src/server.js
+++ src/server.js
@@ -60,9 +60,8 @@ app.get("/api/social/:location", (req, res) => {
 //   console.log(code);
 // })
 
-
-// 비밀번호 업데이트 엔드포인트
-app.post("/api/change-password", async (req, res) => {
+// 비밀번호 및 사용자 정보 업데이트 엔드포인트
+app.post("/api/change-user-info", async (req, res) => {
   try {
     // 현재 사용자의 액세스 토큰 가져오기
     const accessToken = req.headers.authorization.split(" ")[1];
@@ -71,22 +70,22 @@ app.post("/api/change-password", async (req, res) => {
     const decoded = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
     const userId = decoded.no;
 
-    // 새로운 비밀번호 받기
-    const { newPassword } = req.body;
+    // 새로운 비밀번호, 이메일, 전화번호 받기
+    const { newPassword, email, phone } = req.body;
 
     // 새로운 비밀번호를 해싱
     const encryptedPassword = await bcrypt.hash(newPassword, 8);
 
-    // 새로운 비밀번호를 DB에 업데이트
-    const QUERY = "UPDATE users SET user_password = ? WHERE user_no = ?";
-    await db.execute(QUERY, [encryptedPassword, userId]);
+    // 사용자 정보를 DB에 업데이트
+    const UPDATE_QUERY = "UPDATE users SET user_password = ?, user_email = ?, user_tel = ? WHERE user_no = ?";
+    await db.execute(UPDATE_QUERY, [encryptedPassword, email, phone, userId]);
 
     // 성공 응답 전송
-    res.status(200).json({ success: true, message: "비밀번호가 성공적으로 변경되었습니다." });
+    res.status(200).json({ success: true, message: "비밀번호 및 사용자 정보가 성공적으로 변경되었습니다." });
   } catch (error) {
     // 오류가 발생한 경우 클라이언트로 오류 응답 전송
     console.error(error);
-    res.status(500).json({ success: false, message: "Failed to change password" });
+    res.status(500).json({ success: false, message: "Failed to change password and user info" });
   }
 });
 
