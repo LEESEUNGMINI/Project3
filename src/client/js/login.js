e7e3fa0806ed11270b20afc50125f7fc33c7fbaa
koinoniays2 <koinonia_ys@naver.com>
2024-05-14 10:21:47 +0900

    [delete] common 함수

--- src/client/js/login.js
+++ src/client/js/login.js
@@ -22,17 +22,6 @@ eyesa.addEventListener("click", () => {
     }
 });
 
-
-const getParameterByName = (name, url) => {
-  if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, "\\$&");
-  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-    results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return "";
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
-};
-
 const loginFetch = async () => {
   const userId = userIdInput.value;
   const userPassword = userPasswordInput.value;
